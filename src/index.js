2112f633a80675d6e4f138e384c905ad6ac266ff
Arti Villa <artivillachandok@gmail.com>
2017-11-11 15:32:30 -0500

    adding marks with path values

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	acclamation: require('./acclamation'),
+	authority: require('./authority'),
+	certitude: require('./certitude'),
+	doubt: require('./doubt'),
+	elrey: require('./elrey'),
+	exclamationComma: require('./exclamation-comma'),
+	friendlyPeriod: require('./friendly-period'),
+	interrobang: require('./interrobang'),
+	irony: require('./irony'),
+	lovePoint: require('./love-point'),
+	questionComma: require('./question-comma'),
+	rhetoricalQuestion: require('./rhetorical-question'),
+	snarkMark: require('./snark-mark')
+};
