6b4336f1a738812f191722e8c7a05563d7685823
Arti Villa <artivillachandok@gmail.com>
2017-11-11 15:46:03 -0500

    successfully building svg icons in dist/

--- scripts/build-svg.js
+++ scripts/build-svg.js
@@ -1,21 +1,24 @@
-
-var fs = require('fs');
-var path = require('path');
-var punctuations = require('..');
+const fs = require('fs');
+const path = require('path');
+const punctuations = require('../src');
 
 function buildSvg(name) {
-  var contents = punctuations.paths[name];
-  var svg = [
-    '<svg id="' + name + '" xmlns="http://www.w3.org/2000/svg"' +
-    ' viewBox="0 0 32 32" width="32" height="32" fill="currentcolor">',
-    '  <path d="' + contents + '"></path>',
-    '</svg>'
-  ].join('\n');;
-  fs.writeFileSync(path.join(__dirname, '../dist/icons/' + name + '.svg'), svg);
-  console.log(name + '.svg saved');
+	const contents = punctuations[name];
+	const svg = [
+		'<svg id="' +
+			name +
+			'" xmlns="http://www.w3.org/2000/svg"' +
+			' viewBox="0 0 32 32" width="32" height="32" fill="currentcolor">',
+		'  <path d="' + contents + '"></path>',
+		'</svg>'
+	].join('\n');
+	fs.writeFileSync(
+		path.join(__dirname, '../dist/icons/' + name + '.svg'),
+		svg
+	);
+	console.log(name + '.svg saved');
 }
 
-Object.keys(punctuations.paths).forEach(function(key) {
-  buildSvg(key);
+Object.keys(punctuations).forEach(key => {
+	buildSvg(key);
 });
-
